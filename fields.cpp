2638cb5645a14079169b899ea09c222df914d3ea
VasilisIoannou <vasilis895@gmail.com>
2025-03-09 14:09:28 +0200

    Added Field interaction with Charges

--- fields.cpp
+++ fields.cpp
@@ -5,6 +5,7 @@
 
 const float pi = 3.1415926535;
 const float electronCharge = 1.60217663 * pow(10,-19);
+const float CoulombsConst = 8.99 * pow(10,9);
 
 struct str_screen{
 	const int Height = 480;
@@ -13,6 +14,33 @@ struct str_screen{
 	const int fieldH = 48;
 	const int fieldW = 64;
 }screen;
+
+class cl_charge{
+private:
+	float chargeMagnitude;
+	bool sign;
+	float x,y;
+public:
+	cl_charge(float setCM,bool setS,float setX,float setY){
+		chargeMagnitude = setCM;
+		sign = setS;
+		x = setX;
+		y = setY;
+	}
+	float getChargeMagnitude() const { return chargeMagnitude; }
+	bool getSign() const { return sign; }
+	float getX() const { return x; }
+	float getY() const { return y; }
+
+	void draw(SDL_Renderer* renderer){
+		const int r = 5;
+
+		SDL_SetRenderDrawColor(renderer,0,0,255,255);
+		for(float f=0;f<2*pi;f+=pi/10){
+			SDL_RenderPoint(renderer,x+r*sin(f),y+r*cos(f));
+		}
+	}
+}NegCharge(pow(10,-6),false,screen.Width/2,screen.Height/2);
 
 class cl_vector{
 private:
@@ -35,7 +63,20 @@ public:
 		y1 = setM * sin(setPh) + setY;
 	}
 	
-	void update(){
+	void update(float chargeX, float chargeY, float chargeMagnitude, bool chargeSign){
+		float r = sqrt((x0 - chargeX) * (x0 - chargeX) + (y0 - chargeY)*(y0 - chargeY));
+		if(r != 0){
+			magnitude += (CoulombsConst * chargeMagnitude) / (r * r);
+		} else magnitude = 0;
+		if(x0 - chargeX !=0){
+			phase = atan( (y0 - chargeY) / (x0 - chargeX) );
+		} else if( chargeX > x0 ) {
+			phase = pi/2;
+		} else phase = -pi/2;
+		if(!chargeSign){ //The electric field has a negative sign
+			phase =  phase + pi;
+		}
+
 		calculateEndPoints();
 	}
 
@@ -63,14 +104,16 @@ public:
 		while(phase < -2*pi){
 			phase += 2*pi;
 		}
-		update();
+		calculateEndPoints();
 	}
 };
 
 class cl_field{
 private:
 	std::vector<std::vector<cl_vector>> field;
-	
+        
+	std::vector<cl_charge*> chargeList;
+
 	void initialiseVectors(){
 		field.resize(screen.fieldW, std::vector<cl_vector>(screen.fieldH,cl_vector(0,0,0,0)));
 		for(int w = 0;w<screen.fieldW;w++){
@@ -83,6 +126,27 @@ public:
 	cl_field(){
 		initialiseVectors();
 	}
+	void update(){
+		for(auto currentChargeIt = chargeList.begin(); currentChargeIt != chargeList.end(); ++currentChargeIt){
+			cl_charge* currentCharge = *currentChargeIt;
+			float currentChargeMagnitude = currentCharge->getChargeMagnitude();
+			float currentChargeX = currentCharge->getX();
+			float currentChargeY = currentCharge->getY();
+			bool currentChargeSign = currentCharge->getSign();
+			for(int w=0;w<screen.fieldW;w++){
+				for(int h=0;h<screen.fieldH;h++){
+					field[w][h].update(currentChargeX,currentChargeY,currentChargeMagnitude,currentChargeSign);	
+				}
+			}
+		}
+
+
+	}
+	
+	void setCharge(cl_charge* newCharge){
+		chargeList.push_back(newCharge);
+	}
+
 	void draw(SDL_Renderer* renderer){
 		//Draw a vector
 		for(int w=0;w<screen.fieldW;w++){
@@ -96,22 +160,6 @@ public:
 	}
 }field;
 
-class cl_charge{
-private:
-	float chargeMagnitude;
-	bool sign;
-public:
-	cl_charge(float setCM,bool setS){
-		chargeMagnitude = setCM;
-		sign = setS;
-	}
-	float getChargeMagbitude const { return chargeMagnitude; }
-	bool getSign const { return sign; }
-
-	void draw(SDL_Renderer* renderer){
-		SDL_0SetRenderDrawColor(renderer,0,0,255,255);
-	}
-}electron(1,false);
 
 int main(){
 	
@@ -119,6 +167,9 @@ int main(){
 
 	SDL_Window* window = SDL_CreateWindow("SDL3 vector", screen.Width, screen.Height, SDL_WINDOW_RESIZABLE);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr) ;
+
+	field.setCharge(&NegCharge);
+	field.update();
 
 	bool running = true;
 		
@@ -133,8 +184,9 @@ int main(){
 		SDL_RenderClear(renderer);
 		
 		field.draw(renderer);
+		NegCharge.draw(renderer);
 		SDL_RenderPresent(renderer);
-
+		
 		SDL_Delay(1000);
 		
 		

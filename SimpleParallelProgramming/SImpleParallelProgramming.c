ad18d0b4ef58b72f8f109d8853648590014d783b
VasilisIoannou <vasilis895@gmail.com>
2025-03-27 21:40:54 +0200

    red Color

--- SimpleParallelProgramming/SImpleParallelProgramming.c
+++ SimpleParallelProgramming/SImpleParallelProgramming.c
@@ -3,10 +3,17 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <string.h>
+#include <term.h>
+
+#define RED "\033[31m"
+#define RESET "\033[0m"
 
 typedef struct{
         int rows, cols;
         int center_x,center_y;
+	int start_y;
+	int current_option;
 }window;
 
 void window_init(window* self){
@@ -29,17 +36,36 @@ void window_init(window* self){
 
         self->center_x = w.ws_row / 2;
         self->center_y = w.ws_col / 2;
+
+	self->start_y = 5;
+	self->current_option = 1;
 }
 
-void window_print(window* self,int x,int y,char* text){
-        // Clear screen
+void window_print(window* self){
+        //Set Parameters
+	 const int number_of_options = 5;
+	 char options[][32] = {"Parallel Counter","Add Worker","Remove Worker","Details","Exit"};
+	       
+	// Clear screen
         printf("\033[2J");
-
-        //Print Text
-        printf("\033[%d;%dH%s", y, x, text);
-        fflush(stdout); // Ensure output is displayed immediately
+	
+        //Print Screen
+	int x = self->center_x;
+	int y = self->start_y;
+	
+	for(int i=0;i<number_of_options;i++){
+		if(self->current_option == i){
+			printf("\033[%d;%dH%s%s%s",y+i*10,x,RED,options[i] ,RESET);
+		} else {
+			printf("\033[%d;%dH%s",y+i*10,x,options[i]);	
+		}
+       		fflush(stdout); // Ensure output is displayed immediately
+	}
 }
 
+void window_handleInput(window* self,int input){
+	return;
+}
 int main() {
 
         window w;
@@ -47,9 +73,10 @@ int main() {
 
         printf("\033[?25l");  // Hide cursor
         printf("\033[2K");
-        window_print(&w,10,10,"Hello World\n");
-
-        while(1){
+       
+        window_print(&w);
+	
+	while(1){
                 sleep(5);
         }
         return 0;

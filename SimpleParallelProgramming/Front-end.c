2d98a880947657793d5825af44087ee7ca2f975c
VasilisIoannou <vasilis895@gmail.com>
2025-03-28 16:01:59 +0200

    Update Front-end.c

--- SimpleParallelProgramming/Front-end.c
+++ SimpleParallelProgramming/Front-end.c
@@ -31,32 +31,32 @@ enum STATE{
 } current_state = RUNNING;
 
 typedef struct{
-        int rows, cols;
-        int center_x,center_y;
+	int rows, cols;
+	int center_x,center_y;
 	int start_y;
 	int current_option,number_of_options;
 }window;
 
 void window_init(window* self){
-        if (self == NULL) {
-                fprintf(stderr, "Error: NULL window pointer passed to window_init\n");
-                exit(EXIT_FAILURE);
-        }
+	if (self == NULL) {
+			fprintf(stderr, "Error: NULL window pointer passed to window_init\n");
+			exit(EXIT_FAILURE);
+	}
 
-        struct winsize w;
+	struct winsize w;
 
-        if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1 ){
-                perror("ioctl error");
-                // Set default values if ioctl fails
-                self->rows = 24;
-                self->cols = 80;
-        } else {
-                self->rows = w.ws_row;
-                self->cols = w.ws_col;
-        }
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1 ){
+			perror("ioctl error");
+			// Set default values if ioctl fails
+			self->rows = 24;
+			self->cols = 80;
+	} else {
+			self->rows = w.ws_row;
+			self->cols = w.ws_col;
+	}
 
-        self->center_x = w.ws_row / 2;
-        self->center_y = w.ws_col / 2;
+	self->center_x = w.ws_row / 2;
+	self->center_y = w.ws_col / 2;
 
 	self->start_y = 5;
 	self->current_option = 1;
@@ -64,15 +64,21 @@ void window_init(window* self){
 }
 
 void window_print(window* self){
-        //Set Parameters
+	//Set Parameters
 	 char options[][32] = {"Parallel Counter","Add Worker","Remove Worker","Details","Exit"};
 	       
 	// Clear screen
-        printf("\033[2J");
+	printf("\033[2J");
 	
-        //Print Screen
+	//Print Screen
 	int x = self->center_x;
 	int y = self->start_y;
+	
+	printf("\033[%d;%dH%s",y,x,options[0]);
+
+	for(int i=0;i<self->cols-1;i++){
+			printf("\033[%d;%dH%c",y + 3,i,'-');
+	}
 	
 	for(int i=0;i<self->number_of_options;i++){
 		if(self->current_option == i){
@@ -81,6 +87,22 @@ void window_print(window* self){
 			printf("\033[%d;%dH%s",y+i*10,x,options[i]);	
 		}
        		fflush(stdout); // Ensure output is displayed immediately
+	}
+	
+	//Print Help Section
+	const int number_of_help_options = 4;
+	char help_options[][32] = {"Help","Move Up: 'w' and 'k'","Move down: 's', 'j'", "Choose Option: 'e' 'l'"};
+	const int help_offset = 3;
+	const int constant_offset = offset*self->number_of_options;
+
+	for(int i=0;i<self->cols-1;i++){
+			printf("\033[%d;%dH%c",y+constant_offset - 3,i,'-');
+	}
+
+	for(int i=0;i<number_of_help_options;i++){
+			printf("\033[%d;%dH%s",y+constant_offset+help_offset*i,x,help_options[i]);
+
+			fflush(stdout); // Ensure output is displayed immediately
 	}
 }
 void window_executeInput(window* self,char input){
@@ -102,7 +124,7 @@ void window_executeInput(window* self,char input){
 
 void window_handleInput(window* self,char input){
 	if(input == 'j' || input == 's'){
-		if(self->current_option < self->number_of_options) self->current_option++;
+		if(self->current_option < self->number_of_options - 1) self->current_option++;
 	}
 	if(input == 'k' || input == 'w'){
 		if(self->current_option > 1) self->current_option--;
